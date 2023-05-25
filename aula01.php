6f380ee17200fb772fece4d8e8da7d3ef7c66054
Nathalia Molina Da Silva <112526050+Nathy019@users.noreply.github.com>
2023-05-24 22:20:55 -0300

    aula 2
    logicas

---
+++ aula01.php
@@ -0,0 +1,15 @@
+<?php 
+//Operadores relacioais
+
+echo (20 > 30) . " false <br>";#maior
+echo (20 < 30) . " true <br>";#menor
+echo (10 == 10) . " true <br>";#igualdade
+echo (10 != 10) . " false <br>";#diferente
+echo (10 >= 10 ) . " true <br>";#maior ou igual
+echo (10 <= 10) . " true <br>";#menor ou igual
+
+//operadores logicos
+echo (10 == 10 && 10 > 5) . " true <br>";#operador E
+echo (10 == 10 || 10 > 5 ) . " true <br>";#operador OU
+echo (!false) . " true <br>";#operador não
+?>
